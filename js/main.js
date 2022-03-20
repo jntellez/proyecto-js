5dbb743387f2d097d4edae74fe7de535584d782c
Juan Tellez <juantellez916@gmail.com>
2022-03-19 23:14:15 -0600

    slector-theme incluido

--- js/main.js
+++ js/main.js
@@ -1,7 +1,55 @@
-$(function(){
-    $('.slider').bxSlider({
-      mode: 'fade',
-      captions: true,
-      slideWidth: 600
-    });
+$(document).ready(function () {
+
+  // Slider
+  $('.slider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    slideWidth: 600
   });
+
+  // Posts dinamicos
+  var posts = [
+    {
+      title: 'Prueba de titulo 1',
+      date: moment().format('LL'),
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu tellus sapien. Nam efficitur arcu quis nisi tristique, eget lobortis diam fringilla.Interdum et malesuada fames ac ante ipsum primis in faucibus.Sed maximus congue condimentum.Ut quis ex non dolor egestas commodo sit amet non leo.Pellentesque faucibus ex in mollis viverra.Morbi nec ligula at sapien egestas lobortis ac eu leo.Suspendisse interdum, tellus in congue interdum, mauris leo ornare elit, eget finibus tortor lectus id eros.Ut risus libero, faucibus eget massa finibus, luctus consequat nisi.Donec orci nulla, vulputate id libero id, tempus ultrices mauris.Integer tempor, eros sed consequat interdum, enim odio efficitur nunc, nec pretium quam purus eu tellus. Praesent ac eros sagittis, condimentum nulla in, luctus nibh.Praesent efficitur est ac magna fringilla lobortis.Duis non odio erat. Aliquam tempor commodo lorem, fringilla sagittis est fermentum sed.Aenean id lobortis ante. Nullam suscipit euismod erat vitae consectetur.Proin sodales sem a consequat blandit.Aliquam in leo euismod, dignissim sem non, placerat odio.Praesent fermentum nulla non mi ultricies, eget consequat elit laoreet.'
+    },
+    {
+      title: 'Prueba de titulo 2',
+      date: moment().format('LL'),
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu tellus sapien. Nam efficitur arcu quis nisi tristique, eget lobortis diam fringilla.Interdum et malesuada fames ac ante ipsum primis in faucibus.Sed maximus congue condimentum.Ut quis ex non dolor egestas commodo sit amet non leo.Pellentesque faucibus ex in mollis viverra.Morbi nec ligula at sapien egestas lobortis ac eu leo.Suspendisse interdum, tellus in congue interdum, mauris leo ornare elit, eget finibus tortor lectus id eros.Ut risus libero, faucibus eget massa finibus, luctus consequat nisi.Donec orci nulla, vulputate id libero id, tempus ultrices mauris.Integer tempor, eros sed consequat interdum, enim odio efficitur nunc, nec pretium quam purus eu tellus. Praesent ac eros sagittis, condimentum nulla in, luctus nibh.Praesent efficitur est ac magna fringilla lobortis.Duis non odio erat. Aliquam tempor commodo lorem, fringilla sagittis est fermentum sed.Aenean id lobortis ante. Nullam suscipit euismod erat vitae consectetur.Proin sodales sem a consequat blandit.Aliquam in leo euismod, dignissim sem non, placerat odio.Praesent fermentum nulla non mi ultricies, eget consequat elit laoreet.'
+    },
+    {
+      title: 'Prueba de titulo 3',
+      date: moment().format('LL'),
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu tellus sapien. Nam efficitur arcu quis nisi tristique, eget lobortis diam fringilla.Interdum et malesuada fames ac ante ipsum primis in faucibus.Sed maximus congue condimentum.Ut quis ex non dolor egestas commodo sit amet non leo.Pellentesque faucibus ex in mollis viverra.Morbi nec ligula at sapien egestas lobortis ac eu leo.Suspendisse interdum, tellus in congue interdum, mauris leo ornare elit, eget finibus tortor lectus id eros.Ut risus libero, faucibus eget massa finibus, luctus consequat nisi.Donec orci nulla, vulputate id libero id, tempus ultrices mauris.Integer tempor, eros sed consequat interdum, enim odio efficitur nunc, nec pretium quam purus eu tellus. Praesent ac eros sagittis, condimentum nulla in, luctus nibh.Praesent efficitur est ac magna fringilla lobortis.Duis non odio erat. Aliquam tempor commodo lorem, fringilla sagittis est fermentum sed.Aenean id lobortis ante. Nullam suscipit euismod erat vitae consectetur.Proin sodales sem a consequat blandit.Aliquam in leo euismod, dignissim sem non, placerat odio.Praesent fermentum nulla non mi ultricies, eget consequat elit laoreet.'
+    }
+  ];
+
+  posts.forEach(function(item, index) {
+    var post = `
+      <article class="post">
+        <h2>${item.title}</h2>
+        <span class="date">${item.date}</span>
+        <p>
+            ${item.content}
+        </p>
+        <a href="#" class="button-more">Leer mas</a>
+      </article>
+    `
+    $('#posts').append(post);
+  });
+
+  // Selector theme
+  var theme = $('#theme');
+  $('#theme-green').click(function() {
+    theme.attr('href', 'css/green.css');
+  });
+  $('#theme-red').click(function() {
+    theme.attr('href', 'css/red.css');
+  });
+  $('#theme-blue').click(function() {
+    theme.attr('href', 'css/blue.css');
+  });
+
+});
